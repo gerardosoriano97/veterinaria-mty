e04993485e792ae31e37c148942cae73a8ff46f0
Gerardo Soriano <gerardosoriano97@gmail.com>
2022-07-28 16:01:44 -0500

    Estructura inicial

--- app/src/js/app.js
+++
@@ -1,3 +0,0 @@
-import { Hola } from './class/Hola.js';
-
-alert(Hola.saludo());
--- app/src/js/class/Hola.js
+++
@@ -1,9 +0,0 @@
-class Hola{
-    constructor(){}
-
-    static saludo(){
-        return 'Prueba 1';
-    }
-}
-
-export { Hola };
